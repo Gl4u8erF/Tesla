09df410ba9949f72ce4722c158488e9cf9b76f32
Gl4u8erF <128665507+Gl4u8erF@users.noreply.github.com>
2024-11-19 11:49:20 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,18 @@
+//Variáveis//-> Uma forma de identificar quem é quem.
+//Dar um nome pra um pedacinho de código.
+//É como se vc criasse uma pasta, para guardar o que quiser.
+
+//Funções//-> É um trecho de codigo que so execulta quando é chamado.
+
+let formulario = document.querySelector(".formulario")
+
+function aparecerFormulario(){
+    formulario.style.left= "50%"
+
+}
+
+function desaparecerFormulario(){
+     formulario.style.left= "-50%"
+
+}
+
